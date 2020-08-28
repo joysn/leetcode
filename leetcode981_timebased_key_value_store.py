d9fa9b917906c9ec8d9b7fe88a8f6973da8337c5
joysn <joysn1980@yahoo.com>
2020-08-28 09:13:47 +0530

    Add files via upload

---
+++ leetcode981_timebased_key_value_store.py
@@ -0,0 +1,58 @@
+# https://leetcode.com/problems/time-based-key-value-store/
+# 981. Time Based Key-Value Store
+# Create a timebased key-value store class TimeMap, that supports two operations.
+# 1. set(string key, string value, int timestamp)
+
+# Stores the key and value, along with the given timestamp.
+# 2. get(string key, int timestamp)
+
+# Returns a value such that set(key, value, timestamp_prev) was called previously, with timestamp_prev <= timestamp.
+# If there are multiple such values, it returns the one with the largest timestamp_prev.
+# If there are no values, it returns the empty string ("").
+ 
+# Example 1:
+# Input: inputs = ["TimeMap","set","get","get","set","get","get"], inputs = [[],["foo","bar",1],["foo",1],["foo",3],["foo","bar2",4],["foo",4],["foo",5]]
+# Output: [null,null,"bar","bar",null,"bar2","bar2"]
+# Explanation:   
+# TimeMap kv;   
+# kv.set("foo", "bar", 1); // store the key "foo" and value "bar" along with timestamp = 1   
+# kv.get("foo", 1);  // output "bar"   
+# kv.get("foo", 3); // output "bar" since there is no value corresponding to foo at timestamp 3 and timestamp 2, then the only value is at timestamp 1 ie "bar"   
+# kv.set("foo", "bar2", 4);   
+# kv.get("foo", 4); // output "bar2"   
+# kv.get("foo", 5); //output "bar2"   
+
+# Example 2:
+# Input: inputs = ["TimeMap","set","set","get","get","get","get","get"], inputs = [[],["love","high",10],["love","low",20],["love",5],["love",10],["love",15],["love",20],["love",25]]
+# Output: [null,null,null,"","high","high","low","low"]
+
+class TimeMap:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.kv = dict()
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key in self.kv:
+            self.kv[key][timestamp] = value
+        else:
+            t_dict = dict()
+            t_dict[timestamp] = value
+            self.kv[key]= t_dict
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key in self.kv:
+            for k in range(timestamp,-1,-1):
+                if k in self.kv[key]:
+                    return self.kv[key][k]
+        return ""
+        
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
